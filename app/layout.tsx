193a3cde228e9e4ba6934173e1ccd824fb3fed81
chittipriya0010 <chitti1890@gmail.com>
2024-09-13 10:33:44 +0530

    fixed minor errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,28 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import StarCanvas from "@/components/main/StarBackground";
+import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chittipriyas Portfolio-Featuring Space Design",
-  description: "I love space so, why don't include in my portfolio.",
+  description: "This is my portfolio",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarCanvas />
+        <StarsCanvas />
         <Navbar />
         {children}
         <Footer />
